5b7716a01b619d9b64326ffbeba226b508560058
VjRFPS <62533236+VjRFPS@users.noreply.github.com>
2020-04-02 14:53:15 -0300

    Create cadastro.cpp

---
+++ cadastro.cpp
@@ -0,0 +1,436 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	string choose;
+	string user;
+	string senha;
+	
+	cout << "Selecione uma opcao\n\n";
+	cout << "1. Iniciar\n";
+	cout << "2. Sair\n\n";
+
+	cout << "Digite aqui o numero da opcao escolhida: ";
+	cin >> choose;
+	if(choose == "1")
+	{
+		system("cls");
+		cout << "Carregando Data Base...";
+		Sleep (2000);
+		system("cls");				
+	}
+
+	else
+	{
+		cout << "Saindo...";
+		Sleep (2000);
+		exit(0);
+	}
+	
+	cout << "Digite o seu nome de usuario: ";
+	cin >> choose;
+	if(choose == "teste")
+	{
+		cout << "Agora digite a senha: ";
+		cin >> senha;
+		if(senha == "teste")
+		{
+			system("cls");
+			user=choose;
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			string client1;
+			string client2;
+			string client3;
+			string client4;
+			string client5;
+			string client6;
+			string client7;
+			string client8;
+			string client9;
+			string client10;
+			
+			string fone1;
+			string fone2;
+			string fone3;
+			string fone4;
+			string fone5;
+			string fone6;
+			string fone7;
+			string fone8;
+			string fone9;
+			string fone10;
+			
+			//Cliente1
+			cout << "Digite o nome do Cliente 1: ";
+			cin >> client1;
+			cout << "Agora,digite o numero do Cliente 1: ";
+			cin >> fone1;
+			system("pause");
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Deseja Fazer uma busca de clientes?(s/n) ";
+			cin >> choose;
+			if(choose == "s")
+			{
+				system("cls");
+				cout << "Cliente 1:";
+				cout << client1 << "\n";
+				cout << fone1 << "\n\n";
+				system("pause");
+			}
+			
+			
+			//Cliente2
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Digite o nome do Cliente 2: ";
+			cin >> client2;
+			cout << "Agora,digite o numero do Cliente 2: ";
+			cin >> fone2;
+			system("pause");
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Deseja Fazer uma busca de clientes?(s/n) ";
+			cin >> choose;
+			if(choose == "s")
+			{
+				system("cls");
+				cout << "Cliente 1:\n";
+				cout << client1 << "\n";
+				cout << fone1 << "\n\n";
+				cout << "Cliente 2:\n";
+				cout << client2 << "\n";
+				cout << fone2 << "\n\n";
+				system("pause");
+			}
+			
+			//Cliente3
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Digite o nome do Cliente 3: ";
+			cin >> client3;
+			cout << "Agora,digite o numero do Cliente 3: ";
+			cin >> fone3;
+			system("pause");
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Deseja Fazer uma busca de clientes?(s/n) ";
+			cin >> choose;
+			if(choose == "s")
+			{
+				system("cls");
+				cout << "Cliente 1:\n";
+				cout << client1 << "\n";
+				cout << fone1 << "\n\n";
+				cout << "Cliente 2:\n";
+				cout << client2 << "\n";
+				cout << fone2 << "\n\n";
+				cout << "Cliente 3:\n";
+				cout << client3 << "\n";
+				cout << fone3 << "\n\n";
+				system("pause");
+			}
+			
+			//Cliente4
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Digite o nome do Cliente 4: ";
+			cin >> client4;
+			cout << "Agora,digite o numero do Cliente 4: ";
+			cin >> fone4;
+			system("pause");
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Deseja Fazer uma busca de clientes?(s/n) ";
+			cin >> choose;
+			if(choose == "s")
+			{
+				system("cls");
+				cout << "Cliente 1:\n";
+				cout << client1 << "\n";
+				cout << fone1 << "\n\n";
+				cout << "Cliente 2:\n";
+				cout << client2 << "\n";
+				cout << fone2 << "\n\n";
+				cout << "Cliente 3:\n";
+				cout << client3 << "\n";
+				cout << fone3 << "\n\n";
+				cout << "Cliente 4:\n";
+				cout << client4 << "\n";
+				cout << fone4 << "\n\n";
+				system("pause");
+			}
+			
+			//Cliente5
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Digite o nome do Cliente 5: ";
+			cin >> client5;
+			cout << "Agora,digite o numero do Cliente 5: ";
+			cin >> fone5;
+			system("pause");
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Deseja Fazer uma busca de clientes?(s/n) ";
+			cin >> choose;
+			if(choose == "s")
+			{
+				system("cls");
+				cout << "Cliente 1:\n";
+				cout << client1 << "\n";
+				cout << fone1 << "\n\n";
+				cout << "Cliente 2:\n";
+				cout << client2 << "\n";
+				cout << fone2 << "\n\n";
+				cout << "Cliente 3:\n";
+				cout << client3 << "\n";
+				cout << fone3 << "\n\n";
+				cout << "Cliente 4:\n";
+				cout << client4 << "\n";
+				cout << fone4 << "\n\n";
+				cout << "Cliente 5:\n";
+				cout << client5 << "\n";
+				cout << fone5 << "\n\n";
+				system("pause");
+			}
+			
+			//Cliente6
+			cout << "Digite o nome do Cliente 6: ";
+			cin >> client6;
+			cout << "Agora,digite o numero do Cliente 6: ";
+			cin >> fone6;
+			system("pause");
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Deseja Fazer uma busca de clientes?(s/n) ";
+			cin >> choose;
+			if(choose == "s")
+			{
+				system("cls");
+				cout << "Cliente 1:\n";
+				cout << client1 << "\n";
+				cout << fone1 << "\n\n";
+				cout << "Cliente 2:\n";
+				cout << client2 << "\n";
+				cout << fone2 << "\n\n";
+				cout << "Cliente 3:\n";
+				cout << client3 << "\n";
+				cout << fone3 << "\n\n";
+				cout << "Cliente 4:\n";
+				cout << client4 << "\n";
+				cout << fone4 << "\n\n";
+				cout << "Cliente 5:\n";
+				cout << client5 << "\n";
+				cout << fone5 << "\n\n";
+				cout << "Cliente 6:";
+				cout << client6 << "\n";
+				cout << fone6 << "\n\n";
+				system("pause");
+			}
+			
+			
+			//Cliente7
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Digite o nome do Cliente 7: ";
+			cin >> client7;
+			cout << "Agora,digite o numero do Cliente 7: ";
+			cin >> fone7;
+			system("pause");
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Deseja Fazer uma busca de clientes?(s/n) ";
+			cin >> choose;
+			if(choose == "s")
+			{
+				system("cls");
+				cout << "Cliente 1:\n";
+				cout << client1 << "\n";
+				cout << fone1 << "\n\n";
+				cout << "Cliente 2:\n";
+				cout << client2 << "\n";
+				cout << fone2 << "\n\n";
+				cout << "Cliente 3:\n";
+				cout << client3 << "\n";
+				cout << fone3 << "\n\n";
+				cout << "Cliente 4:\n";
+				cout << client4 << "\n";
+				cout << fone4 << "\n\n";
+				cout << "Cliente 5:\n";
+				cout << client5 << "\n";
+				cout << fone5 << "\n\n";
+				cout << "Cliente 6:\n";
+				cout << client6 << "\n";
+				cout << fone6 << "\n\n";
+				cout << "Cliente 7:\n";
+				cout << client7 << "\n";
+				cout << fone7 << "\n\n";
+				system("pause");
+			}
+			
+			//Cliente8
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Digite o nome do Cliente 8: ";
+			cin >> client8;
+			cout << "Agora,digite o numero do Cliente 8: ";
+			cin >> fone8;
+			system("pause");
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Deseja Fazer uma busca de clientes?(s/n) ";
+			cin >> choose;
+			if(choose == "s")
+			{
+				system("cls");
+				cout << "Cliente 1:\n";
+				cout << client1 << "\n";
+				cout << fone1 << "\n\n";
+				cout << "Cliente 2:\n";
+				cout << client2 << "\n";
+				cout << fone2 << "\n\n";
+				cout << "Cliente 3:\n";
+				cout << client3 << "\n";
+				cout << fone3 << "\n\n";
+				cout << "Cliente 4:\n";
+				cout << client4 << "\n";
+				cout << fone4 << "\n\n";
+				cout << "Cliente 5:\n";
+				cout << client5 << "\n";
+				cout << fone5 << "\n\n";
+				cout << "Cliente 6:\n";
+				cout << client6 << "\n";
+				cout << fone6 << "\n\n";
+				cout << "Cliente 7:\n";
+				cout << client7 << "\n";
+				cout << fone7 << "\n\n";
+				cout << "Cliente 8:\n";
+				cout << client8 << "\n";
+				cout << fone8 << "\n\n";
+				system("pause");
+			}
+			
+			//Cliente9
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Digite o nome do Cliente 9: ";
+			cin >> client9;
+			cout << "Agora,digite o numero do Cliente 9: ";
+			cin >> fone9;
+			system("pause");
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Deseja Fazer uma busca de clientes?(s/n) ";
+			cin >> choose;
+			if(choose == "s")
+			{
+				system("cls");
+				cout << "Cliente 1:\n";
+				cout << client1 << "\n";
+				cout << fone1 << "\n\n";
+				cout << "Cliente 2:\n";
+				cout << client2 << "\n";
+				cout << fone2 << "\n\n";
+				cout << "Cliente 3:\n";
+				cout << client3 << "\n";
+				cout << fone3 << "\n\n";
+				cout << "Cliente 4:\n";
+				cout << client4 << "\n";
+				cout << fone4 << "\n\n";
+				cout << "Cliente 5:\n";
+				cout << client5 << "\n";
+				cout << fone5 << "\n\n";
+				cout << "Cliente 6:\n";
+				cout << client6 << "\n";
+				cout << fone6 << "\n\n";
+				cout << "Cliente 7 :\n";
+				cout << client7 << "\n";
+				cout << fone7 << "\n\n";
+				cout << "Cliente 8:\n";
+				cout << client8 << "\n";
+				cout << fone8 << "\n\n";
+				cout << "Cliente 9:\n";
+				cout << client9 << "\n";
+				cout << fone9 << "\n\n";
+				system("pause");
+			}
+			
+			//Cliente10
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Digite o nome do Cliente 10: ";
+			cin >> client10;
+			cout << "Agora,digite o numero do Cliente 10: ";
+			cin >> fone10;
+			system("pause");
+			system("cls");
+			cout << "Olá," << user << ".Bem vindo ao programa teste\n\n";
+			cout << "Deseja Fazer uma busca de clientes?(s/n) ";
+			cin >> choose;
+			if(choose == "s")
+			{
+				system("cls");
+				cout << "Cliente 1:\n";
+				cout << client1 << "\n";
+				cout << fone1 << "\n\n";
+				cout << "Cliente 2:\n";
+				cout << client2 << "\n";
+				cout << fone2 << "\n\n";
+				cout << "Cliente 3:\n";
+				cout << client3 << "\n";
+				cout << fone3 << "\n\n";
+				cout << "Cliente 4:\n";
+				cout << client4 << "\n";
+				cout << fone4 << "\n\n";
+				cout << "Cliente 5:\n";
+				cout << client5 << "\n";
+				cout << fone5 << "\n\n";
+				cout << "Cliente 6:\n";
+				cout << client6 << "\n";
+				cout << fone6 << "\n\n";
+				cout << "Cliente 7:\n";
+				cout << client7 << "\n";
+				cout << fone7 << "\n\n";
+				cout << "Cliente 8:\n";
+				cout << client8 << "\n";
+				cout << fone8 << "\n\n";
+				cout << "Cliente 9:\n";
+				cout << client9 << "\n";
+				cout << fone9 << "\n\n";
+				cout << "Cliente 10:\n";
+				cout << client10 << "\n";
+				cout << fone10 << "\n\n";
+				system("pause");
+			}
+			
+			
+			
+				
+		}
+		else
+		{
+			system("cls");
+			cout << "Senha errada,Tente novamente\n";
+			system("pause");
+			exit(0);
+		}
+		
+		
+	}
+	else
+	{
+	system("cls");
+	cout << "Usuario Incorreto.Tente novamente\n";
+	system("pause");
+	exit(0);
+	}
+	
+	
+	
+	
+}
